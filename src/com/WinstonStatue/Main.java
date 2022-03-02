6763a51b373eae2d0d8dbcf17e481f190d412df1
i-eye <ivan.freeman042@gmail.com>
2022-03-01 20:37:16 -0600

    Adds parts 1,2,3,5

--- src/com/WinstonStatue/Main.java
+++ src/com/WinstonStatue/Main.java
@@ -1,21 +1,25 @@
 package com.WinstonStatue;
 
+
 import java.util.ArrayList;
 import java.util.List;
-
+import java.util.Collections;
 
 public class Main {
 
     public static void main(String[] args) {
-        String[] array = {"gaming", "Risk of Reiner", "Zech Herod", "MLA formating","e"};
+        String[] array = {"gaming", "Risk of Reiner", "Zech Herod", "MLA formating","d","a","c","b"};
 
         ArrayList<String> list = new ArrayList<>(List.of(array));
 
-        System.out.println(firstLetter(list));
-        System.out.println(min(list));
+        //System.out.println(firstLetter(list));
+        //System.out.println(min(list));
+        lessThanTwo(list);
+        //sortCheatMethod(list);
+        sort(list);
     }
 
-    static String firstLetter(ArrayList<String> letters){
+    public static String firstLetter(ArrayList<String> letters){
         StringBuilder returner = new StringBuilder();
         for (String word: letters){
             returner.append(word.charAt(0));
@@ -23,7 +27,7 @@ public class Main {
         return returner.toString();
     }
 
-    static String min(ArrayList<String> val){
+    public static String min(ArrayList<String> val){
         String shortest = "";
         int shortLength = Integer.MAX_VALUE;
         for(String word: val){
@@ -35,5 +39,37 @@ public class Main {
         return shortest;
     }
 
-    static String
+    public static ArrayList<String> lessThanTwo(ArrayList<String> two){
+        for(int i = 0;i<two.size();i++){
+            if(two.get(i).length() > 2){
+                two.remove(i);
+                i--;
+            }
+        }
+        return two;
+    }
+
+    public static void sortCheatMethod(ArrayList<String> words){
+        Collections.sort(words);
+        System.out.println(words);
+    }
+
+    public static void sort(ArrayList<String> words){
+        String temp;
+        for(int i = 0;i<words.size();i++){
+            System.out.println(words);
+            int selected = 0;
+            for(int e = i+1; e<words.size(); e++){
+                if((int)words.get(e).charAt(0) > (int)words.get(i).charAt(0)){
+                    selected = e;
+                }
+                System.out.println(selected);
+            }
+            //System.out.println(selected);
+            temp = words.get(i);
+            words.set(i,words.get(selected));
+            words.set(selected, temp);
+        }
+        System.out.println(words);
+    }
 }
